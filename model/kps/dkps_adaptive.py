edf27573a8260504037900e91afa9d16b73bdb90
David Millard <djm3622@rit.edu>
2026-07-09 15:44:34 -0400

    file stubs for alternative encoder types

--- model/kps/dkps_adaptive.py
+++ model/kps/dkps_adaptive.py
@@ -19,7 +19,7 @@ class KarplusStrongAdaptive(nn.Module):
         omega = torch.linspace(0.0, torch.pi, n_fft // 2 + 1)
         self.register_buffer("z", torch.exp(1j * omega)) 
         
-        encoder_type = "m1" # "s1, m1, l1"
+        encoder_type = "l1" # "s1, m1, l1"
 
         if encoder_type == "s1":
             self.encoder = nn.Sequential(
@@ -56,6 +56,32 @@ class KarplusStrongAdaptive(nn.Module):
                 nn.Linear(32 * 8, 32),
                 nn.GELU(),
                 nn.Linear(32, 1)
+            )
+        elif encoder_type == "l1":
+            self.encoder = nn.Sequential(
+                nn.Conv1d(1, 8, kernel_size=65, stride=4, padding=32),
+                nn.GELU(),
+
+                nn.Conv1d(8, 16, kernel_size=33, stride=4, padding=16),
+                nn.GELU(),
+
+                nn.Conv1d(16, 32, kernel_size=15, stride=4, padding=7),
+                nn.GELU(),
+
+                nn.Conv1d(32, 64, kernel_size=9, stride=4, padding=4),
+                nn.GELU(),
+
+                nn.Conv1d(64, 128, kernel_size=9, stride=4, padding=4),
+                nn.GELU(),
+
+                nn.AdaptiveAvgPool1d(8),
+
+                nn.Flatten(),
+                nn.Linear(128 * 8, 128),
+                nn.GELU(),
+                nn.Linear(128, 32),
+                nn.GELU(),
+                nn.Linear(32, 1),
             )
 
         self.a = a
